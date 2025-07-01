cdc5ee1f5d86323df6576ab7abde8762d47da6ab
EduardoTartas <eduardos.tartas@gmail.com>
2025-06-30 20:21:03 -0400

    refatora o menu para melhorar a responsividade e a interação em dispositivos móveis

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,42 @@
 import './assets/styles/index.css';
 
 const menu = document.querySelector('#menu') as HTMLDivElement;
+let isMenuOpen = false;
 
-let pageSize:number = 0;
-const menuHtml:string = `
-  <li class=""><a href="a">Início</a></li>
-  <li class=""><a href="a">Projetos</a></li>
-  <li class=""><a href="a">Quem somos</a></li>
-  <li class=""><a href="a">Contato</a></li>
+const menuHtml = `
+  <li><a href="a">Início</a></li>
+  <li><a href="a">Projetos</a></li>
+  <li><a href="a">Quem somos</a></li>
+  <li><a href="a">Contato</a></li>
 `;
-const close:string = `<li id="more"><img src="./src/assets/images/close.png" alt=""></li>`;
-const more:string = `<li id="more"><img src="./src/assets/images/more.png" alt=""></li>`;
 
-if (pageSize < 768) {
-  menu.innerHTML = more;
-} else {
-  menu.innerHTML = menuHtml;
+const moreIcon = `<li id="more"><img src="./src/assets/images/more.png" alt=""></li>`;
+const closeIcon = `<li id="more"><img src="./src/assets/images/close.png" alt=""></li>`;
+
+function updateMenu() {
+  const mobile = window.innerWidth < 768;
+  
+  if (mobile) {
+    menu.innerHTML = isMenuOpen ? closeIcon + menuHtml : moreIcon;
+  } else {
+    menu.innerHTML = menuHtml;
+    isMenuOpen = false;
+  }
 }
-window.addEventListener('resize', () => {
-  pageSize = window.innerWidth;
-  console.log(pageSize);
 
+function toggleMenu() {
+  if (window.innerWidth < 768) {
+    isMenuOpen = !isMenuOpen;
+    updateMenu();
+  }
+}
+
+updateMenu();
+
+menu.addEventListener('click', (e) => {
+  if ((e.target as HTMLElement).closest('#more')) {
+    toggleMenu();
+  }
 });
+
+window.addEventListener('resize', updateMenu);
